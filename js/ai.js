d9749469b7c691e2cb41db88a2430cc2c0d4342a
Thomas Pradeilles <doodahprod@hotmail.com>
2019-07-25 20:38:45 -0400

    simple ai

--- js/ai.js
+++ js/ai.js
@@ -38,38 +38,57 @@ const wait = t => new Promise(s => setTimeout(s, t))
 exports.ai = async (context, players) => {
   // Return a direction here, choose wisely
 
-  const { x, y } = players[0];
+  let { x, y } = players[0];
 
   for(i = 0; i < players.length; i++)
   {
+    if(players[i].x < 0 || players[i].x > Context.size)
+      continue;
+
+    if(players[i].y < 0 || players[i].y > Context.size)
+      continue;
+
     Context.grid[players[i].x][players[i].y] = true;
   }
 
+  let weigthW = FindWeight(Context.size, Context.grid, x, y, { x: -1, y: 0 });
+  let weigthE = FindWeight(Context.size, Context.grid, x, y, { x: 1, y: 0 });
+  let weigthN = FindWeight(Context.size, Context.grid, x, y, { x: 0, y: -1 });
+  let weigthS = FindWeight(Context.size, Context.grid, x, y, { x: 0, y: 1 });
 
+  console.log("W " + weigthW + " E " + weigthE + " N "+ weigthN + " S " + weigthS );
 
-  if(x - 1 >= 0 && Context.grid[x - 1][y] == false)
-  {
+  if(weigthW >= weigthE && weigthW >= weigthN && weigthW >= weigthS)
     return WEST;
-  }
 
-  if(x + 1 < context.size && Context.grid[x + 1][y] == false)
-  {
+  if(weigthE >= weigthW && weigthE >= weigthN && weigthE >= weigthS)
     return EAST;
-  }
 
-  if(y + 1 >= 0 && Context.grid[x][y + 1] == false)
-  {
-    return SOUTH;
-  }
-
-  if(y - 1 < context.size && Context.grid[x][y - 1] == false)
-  {
+  if(weigthN >= weigthW && weigthN >= weigthE && weigthN >= weigthS)
     return NORTH;
-  }
+
+  if(weigthS >= weigthN && weigthS >= weigthE && weigthS >= weigthW)
+    return SOUTH;
 
   // always EAST, always right
   //await wait(Math.random()*1000)
   return EAST
+}
+
+const FindWeight = (size, grid, x, y, vector) => {
+  let weight = 0;
+
+  let xToTest = x + vector.x;
+  let yToTest = y + vector.y;
+
+  while(xToTest >= 0 && xToTest < size && yToTest >= 0 && yToTest < size && grid[xToTest][yToTest] == false)
+  {
+    weight++;
+    xToTest += vector.x;
+    yToTest += vector.y;
+  }
+
+  return weight;
 }
 
 exports.name = 'CodingMojo'
